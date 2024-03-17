a62bbe7e776bc3b9623e3314c0089b2523585304
Mohamed Fathy <mofasa.peace@gmail.com>
2024-03-17 11:37:10 +0200

    removed bs from bin

--- .local/bin/importInBlender.py
+++
@@ -1,9 +0,0 @@
-import bpy, sys
-
-if bpy.context.object.mode == 'EDIT':
-    bpy.ops.object.mode_set(mode='OBJECT')
-# delete all selected objects
-bpy.ops.object.delete()
-
-for i in range(4, len(sys.argv)):
-    bpy.ops.import_scene.obj(filepath=sys.argv[i])
